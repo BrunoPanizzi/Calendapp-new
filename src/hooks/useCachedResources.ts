fe090a512ab8d650af5bd15066a052c22516abb7
BrunoPanizzi <brunopanizziq@gmail.com>
2023-01-14 11:26:24 -0300

    added auth state to loading hook

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -1,10 +1,13 @@
 import { FontAwesome } from '@expo/vector-icons'
 import * as Font from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
+import { onAuthStateChanged } from 'firebase/auth'
 import { useEffect, useState } from 'react'
+import { Auth } from '../../firebase'
 
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = useState(false)
+  const [isAuthLoaded, setIsAuthLoaded] = useState(false)
 
   // Load any resources or data that we need prior to rendering the app
   useEffect(() => {
@@ -12,13 +15,16 @@ export default function useCachedResources() {
       try {
         SplashScreen.preventAutoHideAsync()
 
+        onAuthStateChanged(Auth, () => {
+          setIsAuthLoaded(true)
+        })
+
         // Load fonts
         await Font.loadAsync({
           ...FontAwesome.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
         })
       } catch (e) {
-        // We might want to provide this error information to an error reporting service
         console.warn(e)
       } finally {
         setLoadingComplete(true)
@@ -29,5 +35,5 @@ export default function useCachedResources() {
     loadResourcesAndDataAsync()
   }, [])
 
-  return isLoadingComplete
+  return isLoadingComplete && isAuthLoaded
 }
